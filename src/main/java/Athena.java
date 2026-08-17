fb77bae648f2b82dce1686f394a96cb48cb4299e
Jiang Yuzhe <jiangyuzhemail@gmail.com>
2026-08-17 23:12:34 +0800

    Add greet and exit in Athena.java

--- src/main/java/Athena.java
+++ src/main/java/Athena.java
@@ -1,11 +1,25 @@
+/**
+ *
+ * Provides major functions for the chatbot.
+ *
+ * @author f1r8
+ */
 public class Athena {
     public static void main(String[] args) {
+        String name = "Athena";
         String banner = "    _  _____ _   _ _____ _   _    _    \n"
                 + "   / \\|_   _| | | | ____| \\ | |  / \\   \n"
                 + "  / _ \\ | | | |_| |  _| |  \\| | / _ \\  \n"
                 + " / ___ \\| | |  _  | |___| |\\  |/ ___ \\ \n"
-                + "/_/   \\_\\_| |_| |_|_____|_| \\_/_/   \\_\\\n";
+                + "/_/   \\_\\_| |_| |_|_____|_| \\_/_/   \\_\\";
 
+        String underscores = "____________________________________________________________";
+        System.out.println(underscores);
         System.out.println(banner);
+        System.out.println("Hello! I'm " + name + ".");
+        System.out.println("What can I do for you?");
+        System.out.println(underscores);
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println(underscores);
     }
 }
